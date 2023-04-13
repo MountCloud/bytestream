5c034ca4e2c3a83b5c3c386ecc9fc55000e1ea5b
mountcloud <mountcloud@outlook.com>
2023-04-14 00:19:21 +0800

    update

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@
 int main(){
 
     //每个bucket的大小8字节，测试bucket分片工作是否正常所以设置的数值小，正常情况下bucket的大小应该根据业务场景设置
+    //也可以不设置bucket大小，默认1024
+
+    //The size of each bucket is 8 bytes. Test whether the bucket sharding is working properly, so the set value is small. Normally, the size of the bucket should be set according to the business scenario
+    //You can also choose not to set the bucket size, which defaults to 1024
     mc::ByteStream* bs = new mc::ByteStream(8);
     
     //write
